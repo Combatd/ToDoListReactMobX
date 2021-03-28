df1f086bc35535d3d4b2c37fd4fcca35d22c1ab1
Combatd <combatdstar@gmail.com>
2021-03-28 12:25:30 -0700

    Set state of toDoInput in TodoStore

--- src/stores/TodoStore.js
+++ src/stores/TodoStore.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import { observable, action, computed } from 'mobx';
 
 class TodoStore {
+    @observable todoInput = React.createRef();
     @observable filter = 'all';
     @observable beforeEditCache = '';
     @observable idForTodo = 3;
@@ -18,6 +20,36 @@ class TodoStore {
         'editing': false,
       },
     ];
+
+
+    @action addTodo = event => {
+        if (event.key === 'Enter') {
+          const todoInput = this.todoInput.current.value;
+    
+          if (todoInput.trim().length === 0) {
+            return;
+          }
+    
+          this.setState((prevState, props) => {
+            let todos = prevState.todos;
+            let idForTodo = prevState.idForTodo + 1;
+    
+            todos.push({
+              id: idForTodo,
+              title: todoInput,
+              completed: false,
+            });
+    
+            return { todos, idForTodo };
+          });
+    
+          this.todoInput.current.value = '';
+        }
+      }
+
+
+
+
 }
 
 const store = new TodoStore();
