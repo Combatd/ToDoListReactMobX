9033ffd555d5e3bb296bc440dc837053979acf53
Combatd <combatdstar@gmail.com>
2021-03-28 02:35:00 -0700

    Update React to support Hooks for mobx

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { Provider } from 'mobx-react';
+import TodoStore from './stores/TodoStore';
+
+// const Root = {
+//     Provider
+// }
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
